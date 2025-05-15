8b20ad090735b5311bf71cf8fee6692c8feecbbb
renanborges <renan.knx@gmail.com>
2025-05-15 21:18:43 +0100

    Nova tela de Contact Detail, recuperando os dados e criando a funcionalidade de compartilhamento de dados

---
+++ app/src/main/java/com/devspace/recyclerview/ContactDetailActivity.kt
@@ -0,0 +1,48 @@
+package com.devspace.recyclerview
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ContactDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_detail)
+
+        //Recuperando as views:
+        val tvName = findViewById<TextView>(R.id.tv_name)
+        val tvPhone = findViewById<TextView>(R.id.tv_phone)
+        val image = findViewById<ImageView>(R.id.image)
+        val tvShare = findViewById<TextView>(R.id.tv_share)
+
+        //Recuperando os dados da tela anterior
+        val name = intent.getStringExtra("name") //- Passando a mesma chave, nao pode ser diferente
+        val phone = intent.getStringExtra("phone")
+        val icon = intent.getIntExtra("icon", R.drawable.sample8) // Obrigatorio um
+        // valor default, zero ou um drawable
+
+        //Fazendo o biding manualmente
+        tvName.text = name
+        tvPhone.text = phone
+        image.setImageResource(icon)
+
+        tvShare.setOnClickListener {
+            //Intent implicita
+
+            val sendIntent: Intent = Intent().apply{
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "$name $phone")
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+    }
+}
